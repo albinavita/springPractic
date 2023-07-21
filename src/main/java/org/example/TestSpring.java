09e865206bff006bc654c44df8520d21ebcaf67d
Albina <albina.melsitova@mail.ru>
2023-07-21 12:40:39 +0300

    project whith annotation @Component and @Autowired

---
+++ src/main/java/org/example/TestSpring.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Neil Alishev
+ */
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
+
+        context.close();
+    }
+}
